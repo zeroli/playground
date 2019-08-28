f7257c0bdfd1584c684643187f7ad859e1939241
zero.li <zeroliql@gmail.com>
2019-08-28 07:42:22 -0700

    simplify merge sorting algo using sentinel

--- sorting.py
+++ sorting.py
@@ -21,28 +21,40 @@ def mergeSort(data):
         _mergeSort(data, l, p)
         _mergeSort(data, p, r)
 
+        #append a sentinel to simplify code, refer to <<Introducton to algorithms>> book, section 2.3
         data1 = [data[i] for i in xrange(l, p)]
+        data1.append(sys.maxint)
         data2 = [data[i] for i in xrange(p, r)]
+        data2.append(sys.maxint)
         #print 'data1: {}, data2: {}'.format(data1, data2)
 
         i = 0
         j = 0
-        m = 0
-        while i < len(data1) and j < len(data2):
+        m = l
+        while m < r:
             if data1[i] < data2[j]:
-                data[l+m] = data1[i]
+                data[m] = data1[i]
                 i += 1
             else:
-                data[l+m] = data2[j]
+                data[m] = data2[j]
                 j += 1
             m += 1
+        # original implementation code
+        # while i < len(data1) and j < len(data2):
+        #     if data1[i] < data2[j]:
+        #         data[l+m] = data1[i]
+        #         i += 1
+        #     else:
+        #         data[l+m] = data2[j]
+        #         j += 1
+        #     m += 1
 
-        for k in xrange(i, len(data1)):
-            data[l+m] = data1[k]
-            m += 1
-        for k in xrange(j, len(data2)):
-            data[l+m] = data2[k]
-            m += 1
+        # for k in xrange(i, len(data1)):
+        #     data[l+m] = data1[k]
+        #     m += 1
+        # for k in xrange(j, len(data2)):
+        #     data[l+m] = data2[k]
+        #     m += 1
 
     _mergeSort(data, 0, len(data))
     print '>>>\n{}'.format(data)
