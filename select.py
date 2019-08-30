92edd3ab3ef52f5a6a73a972c22564d0f4eda956
zero.li <zeroliql@gmail.com>
2019-08-30 16:58:20 -0700

    add select code to quickly select k-th data with iterative method

--- select.py
+++ select.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 def partition(data, l, r):
     if l >= r: return l
@@ -31,9 +32,22 @@ def select(data, k):
         else:
             return _select(data, k, p+1, r)
 
-    kval = _select(data, k, 0, len(data))
-    print 'k: {}-th value {}'.format(k, kval)
-    return kval
+    def _select_iterative(data, k, l, r):
+        while l < r:
+            p = partition(data, l, r)
+            if k == p:
+                return data[p]
+            elif k < p:
+                r = p
+            else:
+                l = p + 1
+        return data[l]
+
+    kval1 = _select(copy.copy(data), k, 0, len(data))
+    kval2 = _select_iterative(copy.copy(data), k, 0, len(data))
+    assert kval1 == kval2
+    print 'k: {}-th value {}'.format(k, kval1)
+    return kval1
 
 if __name__ == '__main__':
     data = [random.randint(1, 100) for i in xrange(1, 20)]
