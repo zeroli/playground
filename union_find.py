a30147a12ee6c7add638b9ea49baef9b0ac35f75
zero.li <zeroliql@gmail.com>
2019-09-11 00:31:24 -0700

    implement union by height, path comperssion in find, for union-find

--- union_find.py
+++ union_find.py
@@ -5,36 +5,61 @@ import random
 
 class DisjointSet(object):
     def __init__(self, num):
+        """each slot records the parent of current location
+        negative value for root means minus of height of joint tree
+        """
         self.data = [-1] * num
-        """each slot records the parent of current location"""
 
     def union(self, k1, k2):
+        """union by height"""
         print '>>union ({}, {})'.format(k1, k2)
         if k1 == k2: return
         r1 = self.find(k1)
         r2 = self.find(k2)
         if r1 == r2: return
-        self.data[r2] = r1
+        """shadow tree attaches to deeper tree"""
+        if -self.data[r1] > -self.data[r2]: # r1 tree is deeper
+            self.data[r2] = r1
+        else:
+            if self.data[r1] == self.data[r2]: # same height
+                self.data[r2] -= 1
+            self.data[r1] = r2
+
         print self
 
-    def find(self, k1):
-        """find the root where 'k1' resides in"""
-        if self.data[k1] < 0:
-            return k1
+    def find(self, k):
+        """find the root where 'k' resides in"""
+        if k < 0 or k >= len(self.data): return None
+        if self.data[k] < 0:
+            return k
         else:
-            return self.find(self.data[k1])
+            """path compression
+            all traversed nodes will be assigned the root directly
+            """
+            self.data[k] = self.find(self.data[k])
+            return self.data[k]
 
     def __str__(self):
-        s = ','.join([str(d) for d in self.data])
-        return s
+        d = dict()
+        for i in xrange(len(self.data)):
+            r = self.find(i)
+            if r not in d: d[r] = []
+            d[r].append(i)
+        import pprint
+        return pprint.pformat(d)
 
 if __name__ == '__main__':
-    lst = [random.randint(0, 9) for _ in xrange(20)]
+    lst = [random.randint(0, 19) for _ in xrange(20)]
     print '>>random list:\n{}'.format(lst)
 
-    ds = DisjointSet(10)
+    ds = DisjointSet(20)
     i = 0
     while i < 10:
         ds.union(random.choice(lst), random.choice(lst))
         i += 1
-    print '>> ds \n{}'.format(ds)
+    print '>>ds\n{}'.format(ds)
+
+    print '>>bunch of finds'
+    for x in [random.randint(0, 50) for _ in xrange(20)]:
+        print '>>find {} => {}'.format(x, ds.find(x))
+    print '>>ds\n{}'.format(ds)
