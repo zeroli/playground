1ae101adf7f9d46ffd20665517deb80d26293f38
Zero Li <zero.li@asml.com>
2019-10-14 15:09:14 -0700

    move some list problems to list folder

--- leetcode/leetcode_153.cc
+++ leetcode/leetcode_153.cc
@@ -39,4 +39,39 @@ public:
         }
         return nums[l];
     }
+    // if duplicates exist
+    int findMin(vector<int>& nums) {
+        int sz = (int)nums.size();
+        int l = 0, r = sz-1;
+        while (l < r) {
+            if (nums[l] < nums[r]) { // early termination
+                return nums[l];
+            }
+            int mid = l + (r-l)/2;
+            if (nums[mid] == nums[l] and nums[mid] == nums[r]) {
+                // there are 3 cases that could result in this
+                // 1) low => low => jump up => lower => mid
+                // 2) low => low => low => low => mid (all equal)
+                // 3) low => low => jump down => higher => mid
+                while (l < mid) {
+                    if (nums[l] == nums[mid]) { // case 2
+                        l++; // eventually min will be found at right side
+                    } else if (nums[l] < nums[mid]) { // case 3
+                        return nums[l];
+                    } else { // case 1
+                        r = mid;
+                        break; // continue to do again
+                    }
+                }
+            } else {
+                // = check for only 2 values is there(mid=l case)
+                if (nums[mid] >= nums[l]) {  // left is sorted
+                    l = mid + 1; // minimum is at right
+                } else {
+                    r = mid; // minimum is at left
+                }
+            }
+        }
+        return nums[l];
+    }
 };
