bd25e28ce9a6e503085e8d571996a01537f9ecad
zeroli <zeroliql@gmail.com>
2022-08-22 12:24:08 -0700

    insert sorted keys

--- simpledb/btree.h
+++ simpledb/btree.h
@@ -15,6 +15,11 @@
         uint32_t num_cells;  // number of k/v pairs
     };
 */
+
+enum NodeType {
+    NODE_LEAF,
+};
+
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
 const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
@@ -61,6 +66,24 @@ void* leaf_node_value(void* node, uint32_t cell_num)
     return (char*)leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+NodeType get_node_type(void* node)
+{
+    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType)value;
+}
+
+void set_node_type(void* node, NodeType type)
+{
+    uint8_t value = type;
+    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+void initialize_leaf_node(void* node)
+{
+    set_node_type(node, NODE_LEAF);
+    *leaf_node_num_cells(node) = 0;
+}
+
 void print_constants()
 {
     printf("ROW_SIZE: %d\n", ROW_SIZE);
@@ -81,9 +104,5 @@ void print_leaf_node(void* node)
     }
 }
 
-void initialize_leaf_node(void* node)
-{
-    *leaf_node_num_cells(node) = 0;
-}
 
 #endif  // SIMPLEDB_BTREE_H_
--- simpledb/vm.h
+++ simpledb/vm.h
@@ -6,18 +6,28 @@
 
 enum ExecuteResult {
     EXECUTE_SUCCESS,
+    EXECUTE_DUPLICATE_KEY,
     EXECUTE_TABLE_FULL,
 };
 
 ExecuteResult execute_insert(Statement* statement, Table* table)
 {
     void* node = get_page(table->pager, table->root_page_num);
-    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
+    uint32_t num_cells = (*leaf_node_num_cells(node));
+    if (num_cells >= LEAF_NODE_MAX_CELLS) {
         return EXECUTE_TABLE_FULL;
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
+    uint32_t key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+    if (cursor->cell_num < num_cells) {
+        uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if (key_at_index == key_to_insert) {
+            delete cursor;
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
     delete cursor;
 
